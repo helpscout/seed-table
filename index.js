7df1e9998913c69c5b6bdbf89927879b7e40a581
Seed <itsjonq@gmail.com>
2016-10-28 23:58:10 -0400

    Implements seed-publish

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var pathfinder = require('./scripts/pathfinder');
 
 var files = pathfinder(
   require('seed-dash'),
+  require('seed-publish'),
   require('seed-states'),
   require('seed-this'),
   path.join(__dirname, 'scss')
